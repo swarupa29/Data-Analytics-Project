441d7c034e8799ccc8fd5376ab268a34e6d87caf
Suvigya Jain <suvigyajain1@gmail.com>
2021-11-05 19:55:43 +0530

    * finished bureau.csv * Boilerplate for main.py

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from bureau import bureau
 from utils import pipeline
 
 res = 0
-df = pd.read_csv('credit_risk/bureau.csv')
-X=  df[:10000]
-X_test = df[10000:11000]
+main_df = pd.read_csv('credit_risk/application_train.csv')
+test_df = pd.read_csv('credit_risk/application_test.csv')
+train_target = main_df[['TARGET', 'SK_ID_CURR']]
+test_target = test_df['SK_ID_CURR']
 
-def main():
+bureau_df = pd.read_csv('credit_risk/bureau.csv')
+def main() -> None:
 
-    b = bureau()
+    # store results of each table in L1
+    L1 = pd.DataFrame()
+    
+    bureau_joined = bureau_df.merge(train_target, on='SK_ID_CURR', how='right')
+    bureau_model = bureau()
+    bureau_model.fit(bureau_joined)
+    bureau_test = bureau_df.merge(test_target, on='SK_ID_CURR', how='right')
+    res = bureau_model.predict(bureau_test)
+    
+    for col in res.columns:
+        L1[col] = res[col]
 
-    pl = pipeline([
-        b.create()
-    ])
+    L1.to_csv('L1.csv', index=False)
 
-    pl.fit(X)
-    res = pl.predict(X_test)
-    print(res)
+if __name__ == '__main__':
+    main()
